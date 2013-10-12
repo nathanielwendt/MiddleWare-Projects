9ce8c0eaee402ae0e13756c789f4387783bc124a
Raghavendra Srinivasan <ragha@utexas.edu>
2013-10-12 12:31:46 -0500

    added the database and search functionality, without the unique universal IDs

--- src/GUITesterClass.java
+++ src/GUITesterClass.java
@@ -6,7 +6,15 @@ import edu.uta.middleware.guitools.MessageConsole;
 public class GUITesterClass{
 
 	public static void main(String[] args) {
-		MessageConsole.invokeDebugConsole("Debug by raga");
+		MessageConsole.invokeDebugConsole("buyer " + 54 + " console");
+		while(true){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(System.nanoTime());
+		}
 	}
 
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Tree<T> {
 	private Node<T> root;
 
 	public Tree(T rootData) {
-		root = new Node<T>();
-		root.setData(rootData);
+		root = new Node<T>(rootData,null);
 		root.setChildren(new ArrayList<Node<T>>());
 	}
 
 	public Node<T> getRoot(){
 		return root;
 	}
+
+	@Override
+	public String toString() {
+		return "root= " + root;
+	}
+	
+	
 }
