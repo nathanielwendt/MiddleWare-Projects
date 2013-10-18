72e736d4dd3f9c0f108d4b70d1ca2fcc5d449474
nathanielwendt <nwendt22@gmail.com>
2013-10-18 01:05:01 -0500

    Built out the buyer and seller classes to support publishing and subscribing to messages.  GUI should call these methods to send them into the message stream.

--- MiniProject2/src/seller/Seller.java
+++ MiniProject2/src/seller/Seller.java
@@ -3,8 +3,10 @@ import java.io.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import entities.Message;
+import events.*;
  
 public class Seller {
+	public String UUID;
 	public LinkedBlockingQueue<Message> incoming = new LinkedBlockingQueue<Message>();
 	public LinkedBlockingQueue<Message> outgoing = new LinkedBlockingQueue<Message>();
 	
@@ -29,6 +31,35 @@ public class Seller {
 			System.out.println(nextMsg.debugString);		
 		}
     	
+    }
+    
+    //To-Do adapt SaleItem to fit this style, and reformat this method
+    public void PublishSaleItem(String baseString,String modifierString,long timeStamp,double costLowerBound, double costUpperBound){
+    	//SaleItem saleItem = new SaleItem(baseString,modifierString,timeStamp,costLowerBound,costUpperBound);
+    	//saleItem.isPublish = true;
+    	//Message saleNotice = new Message(this.UUID,saleItem);
+    	//this.outgoing.add(saleNotice);
+    }
+    
+    public void PublishBidUpdate(int itemId, double bidUpdatePrice){
+    	BidUpdate bidUpdate = new BidUpdate(itemId,bidUpdatePrice);
+    	bidUpdate.isPublish = true;
+    	Message bidUpdateMsg = new Message(this.UUID,bidUpdate);
+    	this.outgoing.add(bidUpdateMsg);
+    }
+    
+    public void PublishSaleNotice(int itemId, int buyerId, double saleValue){
+    	SaleNotice saleNotice = new SaleNotice(itemId, buyerId, saleValue);
+    	saleNotice.isPublish = true;
+    	Message saleNoticeMsg = new Message(this.UUID,saleNotice);
+    	this.outgoing.add(saleNoticeMsg);
+    }
+    
+    public void SubscribeBid(int itemId){
+    	Bid bid = new Bid(itemId, 0.00);
+    	bid.isPublish = false;
+    	Message bidMsg = new Message(this.UUID,bid);
+    	this.outgoing.add(bidMsg);
     }
 
 }
