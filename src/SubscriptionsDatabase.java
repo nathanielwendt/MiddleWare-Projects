3be0a17822c9f8c343fc94b18588b85a02997bf7
Raghavendra Srinivasan <ragha@utexas.edu>
2013-10-19 02:57:39 -0500

    made the subscriptions database thread-safe in a very primitive way as we will use only 6 threads

--- src/SubscriptionsDatabase.java
+++ src/SubscriptionsDatabase.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * In case we want to scale this application (and need 
+ * nice speed), we should change the way thread-safety
+ * is dealt with. Currently, the approach followed is
+ * very inefficient and very primitive.
+ * 
+ * The search result method can also be further optimized by
+ * using hierarchical hashmaps and also fixing the unnecessary 
+ * for-loops.
+ *
+ */
 public class SubscriptionsDatabase {
 
 	private static SubscriptionsDatabase database = null;
@@ -17,7 +28,7 @@ public class SubscriptionsDatabase {
 	public SubscriptionsDatabase(){
 		this.dataTree = new Tree<SaleItem>(new SaleItem("root","level",new Date().getTime(),0,10));
 	}
-	
+
 	/**
 	 * Returns a list of elements for the given
 	 * filtering scheme.
@@ -89,7 +100,7 @@ public class SubscriptionsDatabase {
 					}
 				}
 			}
-			
+
 			if(modifierStringNode == null){
 				return returnList;
 			}
@@ -152,93 +163,95 @@ public class SubscriptionsDatabase {
 	 * @param item
 	 */
 	public void deleteItemFromDatabase(SaleItem item){
-		String baseString = item.getBaseString();
-		String modifierString = item.getModifierString();
-		int year = getYearFromTimeStamp(item.getTimeStamp());
-		int toBeRemoved = -1;
+		synchronized(dataTree){
+			String baseString = item.getBaseString();
+			String modifierString = item.getModifierString();
+			int year = getYearFromTimeStamp(item.getTimeStamp());
+			int toBeRemoved = -1;
 
-		//base string level
-		ArrayList<Node<SaleItem>> baseStringLevelChildren = (ArrayList<Node<SaleItem>>) dataTree.getRoot().getChildren();
-		Node<SaleItem> baseStringNode = null;
-		int size = baseStringLevelChildren.size();
-		for(int i=0;i<size;i++){
-			Node<SaleItem> node = baseStringLevelChildren.get(i);
-			if(node.getData().getBaseString().trim().equals(baseString.trim())){
-				baseStringNode = node;
-				if(node.getData().equals(item)){
+			//base string level
+			ArrayList<Node<SaleItem>> baseStringLevelChildren = (ArrayList<Node<SaleItem>>) dataTree.getRoot().getChildren();
+			Node<SaleItem> baseStringNode = null;
+			int size = baseStringLevelChildren.size();
+			for(int i=0;i<size;i++){
+				Node<SaleItem> node = baseStringLevelChildren.get(i);
+				if(node.getData().getBaseString().trim().equals(baseString.trim())){
+					baseStringNode = node;
+					if(node.getData().equals(item)){
+						toBeRemoved = i;
+					}
+				}
+			}
+			if(toBeRemoved != -1){
+				baseStringLevelChildren.remove(toBeRemoved);
+				toBeRemoved = -1;
+			}
+			if(baseStringNode == null){
+				return;
+			}
+
+			//modifier string level
+			ArrayList<Node<SaleItem>> modifierStringLevelChildren = (ArrayList<Node<SaleItem>>) baseStringNode.getChildren();
+			Node<SaleItem> modifierStringNode = null;
+			size = modifierStringLevelChildren.size();
+			for(int i=0;i<size;i++){
+				Node<SaleItem> node = modifierStringLevelChildren.get(i);
+				if(node.getData().getBaseString().trim().equals(modifierString.trim())){
+					modifierStringNode = node;
+					if(node.getData().equals(item)){
+						toBeRemoved = i;
+					}
+				}
+			}
+
+			if(toBeRemoved != -1){
+				baseStringLevelChildren.remove(toBeRemoved);
+				toBeRemoved = -1;
+			}
+			if(modifierStringNode == null){
+				return;
+			}
+
+			//year level
+			ArrayList<Node<SaleItem>> yearStringLevelChildren = (ArrayList<Node<SaleItem>>) modifierStringNode.getChildren();
+			Node<SaleItem> yearStringNode = null;
+
+			size = yearStringLevelChildren.size();
+			for(int i=0;i<size;i++){
+				Node<SaleItem> node = yearStringLevelChildren.get(i);
+				if(getYearFromTimeStamp(node.getData().getTimeStamp()) == year){
+					yearStringNode = node;
+					if(node.getData().equals(item)){
+						toBeRemoved = i;
+					}
+				}
+			}
+
+			if(toBeRemoved != -1){
+				baseStringLevelChildren.remove(toBeRemoved);
+				toBeRemoved = -1;
+			}
+			if(yearStringNode == null){
+				return;
+			}
+
+			// cost level
+			ArrayList<Node<SaleItem>> costStringLevelChildren = (ArrayList<Node<SaleItem>>) yearStringNode.getChildren();
+			size = costStringLevelChildren.size();
+
+			for(int i=0;i<size;i++){
+				Node<SaleItem> node = costStringLevelChildren.get(i);
+				if(node.getData().getTimeStamp() == item.getTimeStamp() &&
+						node.getData().getCostUpperBound() == item.getCostUpperBound() &&
+						node.getData().getCostLowerBound() == item.getCostLowerBound()&&
+						node.getData().getUuid().equals(item.getUuid())){
 					toBeRemoved = i;
 				}
 			}
-		}
-		if(toBeRemoved != -1){
-			baseStringLevelChildren.remove(toBeRemoved);
-			toBeRemoved = -1;
-		}
-		if(baseStringNode == null){
-			return;
-		}
 
-		//modifier string level
-		ArrayList<Node<SaleItem>> modifierStringLevelChildren = (ArrayList<Node<SaleItem>>) baseStringNode.getChildren();
-		Node<SaleItem> modifierStringNode = null;
-		size = modifierStringLevelChildren.size();
-		for(int i=0;i<size;i++){
-			Node<SaleItem> node = modifierStringLevelChildren.get(i);
-			if(node.getData().getBaseString().trim().equals(modifierString.trim())){
-				modifierStringNode = node;
-				if(node.getData().equals(item)){
-					toBeRemoved = i;
-				}
+			if(toBeRemoved != -1){
+				costStringLevelChildren.remove(toBeRemoved);
 			}
-		}
-
-		if(toBeRemoved != -1){
-			baseStringLevelChildren.remove(toBeRemoved);
-			toBeRemoved = -1;
-		}
-		if(modifierStringNode == null){
-			return;
-		}
-
-		//year level
-		ArrayList<Node<SaleItem>> yearStringLevelChildren = (ArrayList<Node<SaleItem>>) modifierStringNode.getChildren();
-		Node<SaleItem> yearStringNode = null;
-
-		size = yearStringLevelChildren.size();
-		for(int i=0;i<size;i++){
-			Node<SaleItem> node = yearStringLevelChildren.get(i);
-			if(getYearFromTimeStamp(node.getData().getTimeStamp()) == year){
-				yearStringNode = node;
-				if(node.getData().equals(item)){
-					toBeRemoved = i;
-				}
-			}
-		}
-
-		if(toBeRemoved != -1){
-			baseStringLevelChildren.remove(toBeRemoved);
-			toBeRemoved = -1;
-		}
-		if(yearStringNode == null){
-			return;
-		}
-
-		// cost level
-		ArrayList<Node<SaleItem>> costStringLevelChildren = (ArrayList<Node<SaleItem>>) yearStringNode.getChildren();
-		size = costStringLevelChildren.size();
-
-		for(int i=0;i<size;i++){
-			Node<SaleItem> node = costStringLevelChildren.get(i);
-			if(node.getData().getTimeStamp() == item.getTimeStamp() &&
-					node.getData().getCostUpperBound() == item.getCostUpperBound() &&
-					node.getData().getCostLowerBound() == item.getCostLowerBound()&&
-							node.getData().getUuid().equals(item.getUuid())){
-				toBeRemoved = i;
-			}
-		}
-
-		if(toBeRemoved != -1){
-			costStringLevelChildren.remove(toBeRemoved);
 		}
 	}
 
@@ -248,66 +261,68 @@ public class SubscriptionsDatabase {
 	 * @param item
 	 */
 	public void addItemToDataBase(SaleItem item){
-		String baseString = item.getBaseString();
-		String modifierString = item.getModifierString();
-		int year = getYearFromTimeStamp(item.getTimeStamp());
+		synchronized(dataTree){
+			String baseString = item.getBaseString();
+			String modifierString = item.getModifierString();
+			int year = getYearFromTimeStamp(item.getTimeStamp());
 
-		//base string level
-		ArrayList<Node<SaleItem>> baseStringLevelChildren = (ArrayList<Node<SaleItem>>) dataTree.getRoot().getChildren();
-		Node<SaleItem> baseStringNode = null;
-		for(Node<SaleItem> node : baseStringLevelChildren){
-			if(node.getData().getBaseString().trim().equals(baseString.trim())){
-				baseStringNode = node;
+			//base string level
+			ArrayList<Node<SaleItem>> baseStringLevelChildren = (ArrayList<Node<SaleItem>>) dataTree.getRoot().getChildren();
+			Node<SaleItem> baseStringNode = null;
+			for(Node<SaleItem> node : baseStringLevelChildren){
+				if(node.getData().getBaseString().trim().equals(baseString.trim())){
+					baseStringNode = node;
+				}
 			}
-		}
-		if(baseStringNode == null){
-			baseStringLevelChildren.add(new Node<SaleItem>(item,dataTree.getRoot()));
-			return;
-		}
+			if(baseStringNode == null){
+				baseStringLevelChildren.add(new Node<SaleItem>(item,dataTree.getRoot()));
+				return;
+			}
 
-		//modifier string level
-		ArrayList<Node<SaleItem>> modifierStringLevelChildren = (ArrayList<Node<SaleItem>>) baseStringNode.getChildren();
-		Node<SaleItem> modifierStringNode = null;
-		for(Node<SaleItem> node : modifierStringLevelChildren){
-			if(node.getData().getModifierString().trim().equals(modifierString.trim())){
-				modifierStringNode = node;
+			//modifier string level
+			ArrayList<Node<SaleItem>> modifierStringLevelChildren = (ArrayList<Node<SaleItem>>) baseStringNode.getChildren();
+			Node<SaleItem> modifierStringNode = null;
+			for(Node<SaleItem> node : modifierStringLevelChildren){
+				if(node.getData().getModifierString().trim().equals(modifierString.trim())){
+					modifierStringNode = node;
+				}
 			}
-		}
-		if(modifierStringNode == null){
-			modifierStringLevelChildren.add(new Node<SaleItem>(item,baseStringNode));
-			return;
-		}
+			if(modifierStringNode == null){
+				modifierStringLevelChildren.add(new Node<SaleItem>(item,baseStringNode));
+				return;
+			}
 
-		//year level
-		ArrayList<Node<SaleItem>> yearStringLevelChildren = (ArrayList<Node<SaleItem>>) modifierStringNode.getChildren();
-		Node<SaleItem> yearStringNode = null;
-		for(Node<SaleItem> node : yearStringLevelChildren){
-			if(getYearFromTimeStamp(node.getData().getTimeStamp()) == year){
-				yearStringNode = node;
+			//year level
+			ArrayList<Node<SaleItem>> yearStringLevelChildren = (ArrayList<Node<SaleItem>>) modifierStringNode.getChildren();
+			Node<SaleItem> yearStringNode = null;
+			for(Node<SaleItem> node : yearStringLevelChildren){
+				if(getYearFromTimeStamp(node.getData().getTimeStamp()) == year){
+					yearStringNode = node;
+				}
 			}
-		}
-		if(yearStringNode == null){
-			yearStringLevelChildren.add(new Node<SaleItem>(item,modifierStringNode));
-			return;
-		}
+			if(yearStringNode == null){
+				yearStringLevelChildren.add(new Node<SaleItem>(item,modifierStringNode));
+				return;
+			}
 
-		// cost level
-		ArrayList<Node<SaleItem>> costStringLevelChildren = (ArrayList<Node<SaleItem>>) yearStringNode.getChildren();
-		int size = costStringLevelChildren.size();
-		int toBeChecked = -1;
-		for(int i=0;i<size;i++){
-			Node<SaleItem> node = costStringLevelChildren.get(i);
-			if(node.getData().getTimeStamp() == item.getTimeStamp() &&
-					node.getData().getCostUpperBound() == item.getCostUpperBound() &&
-					node.getData().getCostLowerBound() == item.getCostLowerBound() &&
-					node.getData().getUuid().equals(item.getUuid())){
-				toBeChecked = i;
+			// cost level
+			ArrayList<Node<SaleItem>> costStringLevelChildren = (ArrayList<Node<SaleItem>>) yearStringNode.getChildren();
+			int size = costStringLevelChildren.size();
+			int toBeChecked = -1;
+			for(int i=0;i<size;i++){
+				Node<SaleItem> node = costStringLevelChildren.get(i);
+				if(node.getData().getTimeStamp() == item.getTimeStamp() &&
+						node.getData().getCostUpperBound() == item.getCostUpperBound() &&
+						node.getData().getCostLowerBound() == item.getCostLowerBound() &&
+						node.getData().getUuid().equals(item.getUuid())){
+					toBeChecked = i;
+				}
 			}
+			if(toBeChecked != -1){
+				return;
+			}
+			costStringLevelChildren.add(new Node<SaleItem>(item,yearStringNode));
 		}
-		if(toBeChecked != -1){
-			return;
-		}
-		costStringLevelChildren.add(new Node<SaleItem>(item,yearStringNode));
 	}
 
 	public static int getYearFromTimeStamp(long timestamp){
@@ -321,5 +336,5 @@ public class SubscriptionsDatabase {
 	public String toString() {
 		return "" + dataTree;
 	}
-	
+
 }
